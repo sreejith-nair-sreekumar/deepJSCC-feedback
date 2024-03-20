be6e562df30c87418263d1eccdec98499cc895b2
SS <71664442+sreejith-nair-sreekumar@users.noreply.github.com>
2024-03-20 15:03:08 +0100

    Update jscc.py

--- jscc.py
+++ jscc.py
@@ -61,7 +61,7 @@ class NBatchLogger(tf.keras.callbacks.Callback):
                 )
             )
             self.metric_cache.clear()
-
+print("Hello")
 
 class PSNRsVar(tf.keras.metrics.Metric):
     """Calculate the variance of a distribution of PSNRs across batches
